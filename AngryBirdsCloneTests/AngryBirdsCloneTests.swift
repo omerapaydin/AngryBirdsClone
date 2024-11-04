09e26ff949ecc6663980580b41bc72fd2ac36d52
Ömer Apaydın <omerapaydin90@gmail.com>
2024-11-04 17:17:47 +0300

    Initial Commit

---
+++ AngryBirdsCloneTests/AngryBirdsCloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  AngryBirdsCloneTests.swift
+//  AngryBirdsCloneTests
+//
+//  Created by Ömer on 4.11.2024.
+//
+
+import Testing
+@testable import AngryBirdsClone
+
+struct AngryBirdsCloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
